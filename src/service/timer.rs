2e70c83bb25193dd302b86429bcd915d3455cef4
Jakob Eriksson <jakoberiksson99@hotmail.com>
2025-02-22 00:14:30 +0100

    feat: added a cleanup timer and functionality

--- src/service/timer.rs
+++ src/service/timer.rs
@@ -1,15 +1,37 @@
 use poise::serenity_prelude::{Context as SerenityContext, Http as SerenityHttp};
-use timer::Timer;
 use std::sync::Arc;
 use crate::service::config::{self as ConfigService, AppConfigTrait};
 use crate::service::discord as DiscordService;
 
 // Creates and returns a timer that sleeps for a configured amount and then runs the posture check callout
-pub async fn register_timer(ctx: SerenityContext, http: Arc<SerenityHttp>) -> Timer {
-    let timer = *ConfigService::get_config().timer();
+pub async fn register_timer(ctx: SerenityContext, http: Arc<SerenityHttp>) {
+    let timer_dur = *ConfigService::get_config().timer();
+    //let cleanup_timer_dur = *ConfigService::get_config().cleanup_timer();
+    let cleanup_timer_dur = 5; // 5min
 
     loop {
-        tokio::time::sleep(tokio::time::Duration::from_secs(timer)).await; // Set timer from .env
-        DiscordService::posture_check_callout(&ctx, &http).await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(timer_dur)).await; // Set timer from .env
+        let posture_opt = DiscordService::posture_check_callout(&ctx, &http).await;
+
+        // Register auto-deletion of message if we sent it
+        if posture_opt.is_some() {
+            match posture_opt {
+                Some(posture_res) => {
+                    match posture_res {
+                        Ok(msg_ref) => {
+                            // Register Timer
+                            tokio::time::sleep(tokio::time::Duration::from_secs(cleanup_timer_dur)).await;
+                            DiscordService::delete_message(&http, msg_ref).await;
+                        },
+                        Err(why) => {
+                            println!("Failed to send posture check message. Reason: {}", why);
+                        },
+                    }
+                },
+                None => {
+                    return;
+                },
+            }
+        }
     }
 }
